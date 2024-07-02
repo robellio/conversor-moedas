decebd5055754eded2f7e2458277e81dd4c1b6e9
Robelio Barreto <robeliobasantos@gmail.com>
2024-07-02 20:15:04 -0300

    Implementando a interface para usar o terminal

--- src/main/java/br/com/alura/conversor/ConversorApplication.java
+++ src/main/java/br/com/alura/conversor/ConversorApplication.java
@@ -1,13 +1,18 @@
 package br.com.alura.conversor;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConversorApplication {
+public class ConversorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConversorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Bem vindo ao Conversor de Moedas!");
+	}
 }
